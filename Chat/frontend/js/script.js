b929ee14a199bf328d6e2b70f8f4128ffa195341
JvM3d <145098049+JvM3d@users.noreply.github.com>
2024-04-02 23:00:25 -0300

    Update script.js

--- Chat/frontend/js/script.js
+++ Chat/frontend/js/script.js
@@ -80,7 +80,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-aps-jj44.onrender.com")
     websocket.onmessage = processMessage
 
 }
